4b0eafc76bb3e1d8102d546746d15742ded6ccde
mizusaki kazuteru <k_mizusaki@plusize.co.jp>
2016-03-04 10:43:25 +0900

    簡易ツイッター

---
+++ src/chapter6/service/UserService.java
@@ -0,0 +1,71 @@
+package chapter6.service;
+
+import static chapter6.utils.ClosebleUtil.*;
+import java.io.ByteArrayOutputStream;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.User;
+
+import com.mysql.jdbc.Connection;
+
+/**
+ * Servlet implementation class UserService
+ */
+@WebServlet("/UserService")
+public class UserService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public UserService() {
+
+    	public void register(User user){
+
+    		Connection conection();
+    	try{
+    		connection = getConnection();
+
+    		Stirng encPassword = CipherUtil.encrypt(user.getPassword());
+    		user.setPassword(encPassword);
+
+    		setDefaultIcon(user);
+
+    		UserDao userDao = new UserDao();
+    		userDao.insert(connection, user);
+
+    		commit(connection);
+    	}catch (RuntimeException e){
+    		rollback(connection);
+    		throw e;
+    	}finally{
+    		close(connection;)
+    	}
+    }
+
+ private void setDefaultIcon(User user){
+
+	 InputStream is = null;
+	 try{
+		 long randomNum = System.currentTimeMills() % 5;
+		 String filePath = "/duke_" + randomNum + ".jpg";
+		 is = UserService.class.getResourceAsStream(filePath);
+		 ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		 StreamUtil.copy(is, baos);
+		 user.setIcon(baos.toByteArray());
+	 }finally{
+		 close(is);
+	 }
+	 }
+ }
+
+
+
+
